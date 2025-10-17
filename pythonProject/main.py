784d5f429dd2ac44fa79628b747584238c441a32
mnhoang133 <mnhoangnevn@gmail.com>
2025-10-17 09:19:50 +0000

    3.3

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -2,3 +2,10 @@ print('I`m a student')
 
 x = 1 / 7
 print(f"{x:.5f}")
+
+a = int(input("Nhập số nguyên thứ nhất: "))
+b = int(input("Nhập số nguyên thứ hai: "))
+
+tong = a + b
+
+print("Tổng của hai số là:", tong)
