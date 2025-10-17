d84994af277f4e1eba890d7bc69195ab327845d7
XuanHoang380 <hoang071006@gmail.com>
2025-10-17 16:55:38 +0700

    3.5
    Added functionality to write results to 'output.txt' and handle potential errors.

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -12,6 +12,7 @@ print("Tổng của hai số là:", tong)
 
 # Bài tập 4: Nhập tên file, đọc và in nội dung nếu có, báo lỗi nếu không tồn tại
 filename = input("Nhập tên file: ")
+
 try:
     with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
@@ -19,3 +20,14 @@ try:
         print(content)
 except FileNotFoundError:
     print("Lỗi: File không tồn tại.")
+
+with open("output.txt", "w", encoding="utf-8") as f:
+    f.write("I`m a student\n")
+    f.write(f"{x:.5f}\n")
+
+    try:
+        f.write(f"Tổng của hai số là: {tong}\n")
+    except NameError:
+        f.write("Không có kết quả do lỗi nhập liệu.\n")
+
+print(" Kết quả đã được ghi vào file output.txt")
